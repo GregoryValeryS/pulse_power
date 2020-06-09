b9623ecdbf0a6c61339e0d643d8092a533ac28df
Gregory Valery Skvortsov <GregoryValeryS@gmail.com>
2020-06-09 16:49:20 +0500

    поправил баг

--- pyside_program/main.py
+++ pyside_program/main.py
@@ -1,4 +1,6 @@
-# Hook logic
+import sys
+from PyQt5 import QtWidgets
+from ui import Ui_Pulse
 
 
 def bp_rasshet():
@@ -13,7 +15,7 @@ def bp_rasshet():
                               'учитывающим погрешность измерений, произведён успешно.')
         value_vattmeter = float(value_vattmeter)
         ui.line_vattmeter.setText(str(value_vattmeter))
-        if ui.combo_mode.currentIndex() == 0:                   # КП-3
+        if ui.combo_mode.currentIndex() == 0:  # КП-3
             signal_duration_floor = 1
             signal_duration_ceil = 1
             pulse_repetition_rate = 1000
@@ -22,26 +24,26 @@ def bp_rasshet():
             attenuation_Dp_ampl = 0
             attenuation_Dp_power = 22
             station = 'КП-3'
-        else:                                                   # КП-2
-            station = 'КП-2'                 # к КП-2 подходит только этот переход
+        else:  # КП-2
+            station = 'КП-2'  # к КП-2 подходит только этот переход
             performance_ratio_Ke_floor = 0.93
             performance_ratio_Ke_ceil = 1.05
             attenuation_Dp_ampl = 22.7
             attenuation_Dp_power = 0.5
-            if ui.combo_mode.currentIndex() == 1:               # КП-2: 7.5, 10, 15, 60
+            if ui.combo_mode.currentIndex() == 1:  # КП-2: 7.5, 10, 15, 60
                 signal_duration_floor = 0.1
                 signal_duration_ceil = 0.15
                 pulse_repetition_rate = 1500
             else:
                 signal_duration_floor = 0.5
-                if ui.combo_mode.currentIndex() == 4:           # КП-2: 240, 400 обзор сектор
+                if ui.combo_mode.currentIndex() == 4:  # КП-2: 240, 400 обзор сектор
                     signal_duration_ceil = 0.81
                     pulse_repetition_rate = 375
                 else:
                     signal_duration_ceil = 0.73
-                    if ui.combo_mode.currentIndex() == 2:       # КП-2: 120
+                    if ui.combo_mode.currentIndex() == 2:  # КП-2: 120
                         pulse_repetition_rate = 375
-                    else: # ui.combo_mode.currentIndex() == 3   # КП-2: 240, 400 обзор круг
+                    else:  # ui.combo_mode.currentIndex() == 3   # КП-2: 240, 400 обзор круг
                         pulse_repetition_rate = 400
 
         ui.LCD_signal_duration_floor.display(signal_duration_floor)
@@ -50,15 +52,15 @@ def bp_rasshet():
         ui.LCD_ke_floor.display(performance_ratio_Ke_floor)
         ui.LCD_ke_ceil.display(performance_ratio_Ke_ceil)
 
-        if ui.combo_element.currentIndex() == 0:            # КП-3:   Fн = 8.24 - 12.05 ГГц 5.433.022
+        if ui.combo_element.currentIndex() == 0:  # КП-3:   Fн = 8.24 - 12.05 ГГц 5.433.022
             coaxial_transition_utilization_rate_a = 0.03
             converter_reflectance_module = 0.23
             element = 'КП-3'
-        elif ui.combo_element.currentIndex() == 1:          # КП-3:        Fн = 4 - 10 ГГц      5.433.021
+        elif ui.combo_element.currentIndex() == 1:  # КП-3:        Fн = 4 - 10 ГГц      5.433.021
             coaxial_transition_utilization_rate_a = 0.01
             converter_reflectance_module = 0.167
             element = 'КП-3'
-        else: # ui.combo_element.currentIndex() == 2        # КП-2: Fн = 12.05 - 17.44 ГГц 5.433.023
+        else:  # ui.combo_element.currentIndex() == 2        # КП-2: Fн = 12.05 - 17.44 ГГц 5.433.023
             coaxial_transition_utilization_rate_a = 0.02
             converter_reflectance_module = 0.286
             element = 'КП-2'
@@ -73,9 +75,9 @@ def bp_rasshet():
         # удаляем пробелы, запятые меняем на точки
         input_atten_power = ui.line_atten_power.text().replace(' ', '').replace(',', '.')
 
-
         if input_atten_power:
-            if input_atten_power.isdigit() or (input_atten_power.count('.') == 1 and input_atten_power.replace('.', '').isdigit()):
+            if input_atten_power.isdigit() or (
+                    input_atten_power.count('.') == 1 and input_atten_power.replace('.', '').isdigit()):
                 # input_atten_ampl состоит из цифр или  если в поле одна точка не в начале и не в конце, a остальные символы - цифры:
                 attenuation_Dp_power = float(input_atten_power)
                 ui.line_atten_power.setText(str(attenuation_Dp_power))
@@ -86,7 +88,8 @@ def bp_rasshet():
         # удаляем пробелы, запятые меняем на точки
 
         if input_atten_ampl:
-            if input_atten_ampl.isdigit() or (input_atten_ampl.count('.') == 1 and input_atten_ampl.replace('.', '').isdigit()):
+            if input_atten_ampl.isdigit() or (
+                    input_atten_ampl.count('.') == 1 and input_atten_ampl.replace('.', '').isdigit()):
                 # input_atten_ampl состоит из цифр или
                 # если в поле одна точка не в начале и не в конце, a остальные символы - цифры:
                 attenuation_Dp_ampl = float(input_atten_ampl)
@@ -95,20 +98,26 @@ def bp_rasshet():
                 value_vattmeter = 0
                 ui.txt_status.setText('Ошибка формата данных в "4. Ослабление по амплитуде".')
 
-        attenuation_ampl = 10**(attenuation_Dp_ampl/20)
-        attenuation_power = 10**(attenuation_Dp_power/20)
+        attenuation_ampl = 10 ** (attenuation_Dp_ampl / 20)
+        attenuation_power = 10 ** (attenuation_Dp_power / 20)
         attenuation_tract = attenuation_power * attenuation_ampl
 
         ui.LCD_atten_ampl.display(attenuation_ampl)
         ui.LCD_atten_power.display(attenuation_power)
         ui.LCD_atten_tract.display(attenuation_tract)
 
-        pulse_power_ceil = (value_vattmeter * attenuation_tract) / (signal_duration_floor * 10**(-6) * pulse_repetition_rate * (performance_ratio_Ke_floor - 4*coaxial_transition_utilization_rate_a**2) * (1 - converter_reflectance_module**2))
-        pulse_power_floor = (value_vattmeter * attenuation_tract) / (signal_duration_ceil * 10**(-6) * pulse_repetition_rate * (performance_ratio_Ke_ceil - 4*coaxial_transition_utilization_rate_a**2) * (1 - converter_reflectance_module**2))
-        pulse_power_mean = (pulse_power_ceil + pulse_power_floor)/2
-        pulse_power_range = (pulse_power_ceil - pulse_power_floor)/2
-        ui.pulse_power.display(pulse_power_mean/1000)
-        ui.range_pulse_power.display(pulse_power_range/1000)
+        pulse_power_ceil = (value_vattmeter * attenuation_tract) / (
+                signal_duration_floor * 10 ** (-6) * pulse_repetition_rate * (
+                performance_ratio_Ke_floor - 4 * coaxial_transition_utilization_rate_a ** 2) * (
+                        1 - converter_reflectance_module ** 2))
+        pulse_power_floor = (value_vattmeter * attenuation_tract) / (
+                signal_duration_ceil * 10 ** (-6) * pulse_repetition_rate * (
+                performance_ratio_Ke_ceil - 4 * coaxial_transition_utilization_rate_a ** 2) * (
+                        1 - converter_reflectance_module ** 2))
+        pulse_power_mean = (pulse_power_ceil + pulse_power_floor) / 2
+        pulse_power_range = (pulse_power_ceil - pulse_power_floor) / 2
+        ui.pulse_power.display(pulse_power_mean / 1000)
+        ui.range_pulse_power.display(pulse_power_range / 1000)
 
         if pulse_power_mean > 500000:
             ui.txt_status.setText('Расчёт импульсной мощности произведён, но полученное значение '
@@ -144,17 +153,8 @@ def bp_clear():
 
 
 def main():
-    ui.button_Clear.clicked.connect(bp_clear)  # очистка при нажатии кнопка Clear
-    ui.button_Calculation.clicked.connect(bp_rasshet)  # присоединим к нажатию кнопки Rasschet функцию bp_rasshet
-    ui.button_Manual.clicked.connect(bp_manual)  # присоединим к нажатию кнопки Методичка функцию bp_clear
-    # Run main loop
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    import sys
-    from PyQt5 import QtWidgets
-    from ui import Ui_Pulse
+    global ui
+    """Функция инициализации приложения"""
     # Create application - инициализация приложения
     app = QtWidgets.QApplication(sys.argv)
     # Create form and init UI - инициализация формы
@@ -163,4 +163,12 @@ if __name__ == '__main__':
     ui.setupUi(Pulse)
     Pulse.show()
 
+    ui.button_Clear.clicked.connect(bp_clear)  # очистка при нажатии кнопка Clear
+    ui.button_Calculation.clicked.connect(bp_rasshet)  # присоединим к нажатию кнопки Rasschet функцию bp_rasshet
+    # сейчас мне стыдно за такие названия кнопок, типа Rasschet, но переписывать не буду
+    ui.button_Manual.clicked.connect(bp_manual)  # присоединим к нажатию кнопки Методичка функцию bp_clear
+
+    sys.exit(app.exec_())  # Run main loop
+
+if __name__ == '__main__':
     main()
